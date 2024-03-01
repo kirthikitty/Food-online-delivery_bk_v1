d65be3242506c4a2091a8a11c032f2c634c0ce15
kirthikitty <kiruthiramesh262@gmail.com>
2024-03-01 10:39:58 +0530

    Login add

---
+++ src/main/java/com/example/FoodOrderBack/model/Register.java
@@ -0,0 +1,49 @@
+package com.example.FoodOrderBack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Register {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private int password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	
+}
